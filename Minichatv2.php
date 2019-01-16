b6420771a92c8be89d12d6ddf0f1525bac094f93
TimoHemard <45656673+TimoHemard@users.noreply.github.com>
2019-01-16 16:11:40 +0100

    Update Minichatv2.php

--- Minichatv2.php
+++ Minichatv2.php
@@ -1,27 +1,21 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-        <title>Mini-chat</title>
-	</head>
-	<style>
-	form
-	{
-		text-align: center,
-	}
-	</style>
-	<body>
-	
-	<table border="0" align="center" cellpadding="3" cellspacing="3" 
-	<form action="minichat_post.php" method="post">
-		<p>
-		<p>Laissez-nous un message ;)</p>
-		<label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo"/><br />
-		<label for="texte">Message</label> : <input type="text" name="texte" id="texte"/><br />
-		<input type="submit" value="Envoyer" />
-		</p>
-	</form>
+<head>
+<title>Mini-chat</title>
 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-
+8859-1" />
+
+</head>
+<body>
+<form action="minichat_post.php" method="post">
+<p>
+<label for="pseudo">Pseudo</label> : <input type="text"
+name="pseudo" id="pseudo" /><br />
+<label for="texte">Texte</label> : <input
+type="text" name="texte" id="texte" /><br />
+<input type="submit" value="Envoyer" />
+</form>
 <?php 
 //Connexion à la base de donnée 
 try
@@ -34,14 +28,14 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 // Récupération des 10 derniers messages 
-/* $reponse = $bdd->query('SELECT pseudo FROM table_news ORDER BY ID DESC LIMIT 0, 10'); */
-$reponse = $bdd->query('SELECT * FROM table_news ORDER BY ID DESC LIMIT 0, 10');
+ $reponse = $bdd->query('SELECT pseudo, texte FROM table_news ORDER BY ID DESC LIMIT 0, 10');
+
 	echo '<p>D\'autres ont aussi laissé leurs avis sur ce site :</p>';
 // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
 while ($donnees = $reponse->fetch())
 {
-	echo '<p> <strong>' . htmlspecialchars($donnees['pseudo']) .'</p>';
-	echo '<p>.'  . htmlspecialchars($donnees['texte']) . '</p>';
+	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) .
+	'</strong> : ' . htmlspecialchars($donnees['texte']) . '</p>';
 }
 $reponse->closeCursor();
 ?>
