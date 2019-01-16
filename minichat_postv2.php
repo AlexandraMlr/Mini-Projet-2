ddc575ccfdf437b06fd160970c42e91e3707f9e4
TimoHemard <45656673+TimoHemard@users.noreply.github.com>
2019-01-16 16:12:00 +0100

    Update minichat_postv2.php

--- minichat_postv2.php
+++ minichat_postv2.php
@@ -1,19 +1,17 @@
-
 <?php
-// Connexion à la base de données 
-try 
+// Connexion à la base de données
+try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=ma_base', 'root', '');
 	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(Exception $e)
-{		
-		die('Erreur : '.$e->getMessage());
+{
+        die('Erreur : '.$e->getMessage());
 }
-	
 // Insertion du message à l'aide d'une requête préparée
 $req = $bdd->prepare('INSERT INTO table_news (pseudo, texte) VALUES(?, ?)');
 $req->execute(array($_POST['pseudo'], $_POST['texte']));
-// Redirecton du visiteur vers le page du minichat 
-	echo 'Merci de ta participation !';
+// Redirection du visiteur vers la page du minichat
+header('Location: minichat.php');
 ?>
